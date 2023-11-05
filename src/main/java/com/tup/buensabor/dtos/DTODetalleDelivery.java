0478a0ca4432f49740e07575297b466b9a157363
Camila Citro <97923424+camicitro@users.noreply.github.com>
2023-11-05 19:42:00 -0300

    borrar dto no necesario

--- src/main/java/com/tup/buensabor/dtos/DTODetalleDelivery.java
+++
@@ -1,8 +0,0 @@
-package com.tup.buensabor.dtos;
-
-public class DTODetalleDelivery {
-    private String nombre;
-    private String apellido;
-    private String direccionEntrega;
-    private Integer telefono;
-}
